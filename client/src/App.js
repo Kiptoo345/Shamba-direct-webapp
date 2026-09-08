ec6d04e132ccdf0a9c96429713bd9eaedb8075a1
Victor Kiptoo <kimutaiv345@gmail.com>
2026-09-08 15:48:00 +0300

    Added React Frontend

---
+++ client/src/App.js
@@ -0,0 +1,12 @@
+import ProductsTable from './ProductsTable';
+
+function App() {
+  return (
+    <div>
+      <h1>Shamba Direct — Products</h1>
+      <ProductsTable />
+    </div>
+  );
+}
+
+export default App;
